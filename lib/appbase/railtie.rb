c3be80ef5cce4a5d7b2186ee3003a983414eaf57
Mike He <i@bestmike007.com>
2014-12-26 04:06:41 +0000

    Refactoring: extract initializers from appbase railtie engine.

--- lib/appbase/railtie.rb
+++ lib/appbase/railtie.rb
@@ -11,75 +11,156 @@ module AppBase
     
     class << self
       
-      initialized = false
-      config = nil
-      hooks = []
+      class RpcMethodInitializer
+        
+        def initialize(config)
+          @model = config[:model]
+          @method = config[:method]
+          @auth = config[:auth]
+        end
+        
+        def init
+          pre_init
+          add_controller_stub
+          add_route
+        end
+        
+        private
+        def pre_init
+          model_name = @model.name
+          if !@model.respond_to?(@method)
+            raise "#{model_name} does not respond to #{@method}."
+          end
+        end
+        
+        def add_controller_stub
+          bound_method = @model.method @method
+          AppBaseController.add_rpc_method_stub(bound_method, @auth)
+        end
+        
+        def add_route
+          model_name_underscore = AppBase.underscore @model.name
+          method_name = @method
+          AppBase::Engine.routes.append do
+            post "/#{model_name_underscore}/#{method_name}" => "app_base#rpc_#{model_name_underscore}_#{method_name}"
+          end
+        end
+      end
       
-      define_method :bootstrap do |app_config|
-        return if initialized
-        config = app_config.appbase
+      class CrudInitializer
         
-        # initialize user identity
-        if config.user_identity.nil?
-          raise "AppBase configuration error: please use `config.appbase.user_identity = :UserIdentity` to specify the user identity model; and implement UserIdentity.authenticate_by_token(user, token):UserIdentity method."
+        def initialize(model, op)
+          @model = model
+          @op = op
+          @http_methods = { create: :post, update: :put, delete: :delete, query: :get }
         end
-        user_identity = Object.const_get config.user_identity.to_sym
-        if !user_identity.respond_to?(:authenticate_by_token) || user_identity.method(:authenticate_by_token).parameters.count != 2
-          raise "It's required to implement UserIdentity.authenticate_by_token(user, token):UserIdentity method."
+        
+        def init
+          pre_init
+          add_controller_stub
+          add_route
         end
-        AppBase::Engine::UserIdentity = config.user_identity.to_s.extend AppBase::StringExtension
-        AppBaseController.define_useridentity config.user_identity, config.token_store, config.token_key_user, config.token_key_session
         
-        http_methods = { create: :post, update: :put, delete: :delete, query: :get }
+        private
+        def pre_init
+          raise "Unexpected crud operation: #{@op}" if !@http_methods.has_key?(@op)
+        end
         
-        # initialize crud stubs
-        AppBase::Registry.each_crud config.models do |model, op|
-          raise "Unexpected crud operation: #{op}" if !http_methods.has_key?(op)
-          model_name_underscore = AppBase.underscore model.name
-          AppBaseController.send "add_#{op}_stub".to_sym, model
+        def add_controller_stub
+          AppBaseController.send "add_#{@op}_stub".to_sym, @model
+        end
+        
+        def add_route
+          model_name_underscore = AppBase.underscore @model.name
           url_path = "/#{model_name_underscore}"
-          url_path += "/:id" if op == :update || op == :delete
+          url_path += "/:id" if @op == :update || @op == :delete
+          http_method = @http_methods[@op]
+          op = @op
           AppBase::Engine.routes.append do
-            match url_path, to: "app_base##{op}_#{model_name_underscore}", via: http_methods[op]
+            match url_path, to: "app_base##{op}_#{model_name_underscore}", via: http_method
           end
         end
-        
-        # initialize rpc stubs
-        AppBase::Registry.each_rpc do |r|
-          if !r[:model].respond_to? r[:method]
-            raise "#{r[:model].name} does not respond to #{r[:method]}."
-          end
-          bound_method = r[:model].method r[:method]
-          AppBaseController.add_rpc_method_stub(bound_method, r[:auth])
-          AppBase::Engine.routes.append do
-            post "/#{AppBase.underscore r[:model].name}/#{r[:method]}" => "app_base#rpc_#{AppBase.underscore r[:model].name}_#{r[:method]}"
-          end
-        end
-        
-        # finalize appbase routes
-        AppBase::Engine.routes.draw do
-          get "/appbase_version" => "app_base#version"
-        end
-        
-        # after initialized
-        blocks, hooks = hooks, []
-        blocks.each do |block|
-          block.call
-        end
-        initialized = true
         
       end
       
-      define_method :after_initialized do |&block|
-        if initialized
-          block.call
-        else
-          hooks << block
+      class AppBaseEngineInitializer
+        
+        def initialize
+          @initialized = false
+          @hooks = []
+        end
+        
+        def bootstrap(app_config)
+          return if @initialized
+          @config = app_config.appbase
+          initialize_user_identity
+          initialize_crud_stubs
+          initialize_rpc_stubs
+          finilize_routes
+          post_initialize
+          @initialized = true
+        end
+        
+        def after_initialized(&block)
+          if @initialized
+            block.call
+          else
+            @hooks << block
+          end
+        end
+        
+        private
+        
+        def pre_init_user_identity
+          if @config.user_identity.nil?
+            raise "AppBase configuration error: please use `config.appbase.user_identity = :UserIdentity` to specify the user identity model; and implement UserIdentity.authenticate_by_token(user, token):UserIdentity method."
+          end
+          user_identity = Object.const_get @config.user_identity.to_sym
+          if !user_identity.respond_to?(:authenticate_by_token) || user_identity.method(:authenticate_by_token).parameters.count != 2
+            raise "It's required to implement UserIdentity.authenticate_by_token(user, token):UserIdentity method."
+          end
+        end
+        
+        def initialize_user_identity
+          pre_init_user_identity
+          AppBase::Engine.const_set :UserIdentity, @config.user_identity.to_s.extend(AppBase::StringExtension)
+          AppBaseController.define_useridentity @config.user_identity, @config.token_store, @config.token_key_user, @config.token_key_session
+        end
+        
+        def initialize_crud_stubs
+          AppBase::Registry.each_crud @config.models do |model, op|
+            CrudInitializer.new(model, op).init
+          end
+        end
+        
+        def initialize_rpc_stubs
+          AppBase::Registry.each_rpc do |r|
+            RpcMethodInitializer.new(r).init
+          end
+        end
+        
+        def finilize_routes
+          AppBase::Engine.routes.draw do
+            get "/appbase_version" => "app_base#version"
+          end
+        end
+        
+        def post_initialize
+          blocks, @hooks = @hooks, []
+          blocks.each do |block|
+            block.call
+          end
         end
       end
       
-      define_method :config do
-        config
+      def bootstrap(config)
+        @initializer = AppBaseEngineInitializer.new if !@initializer
+        @initializer.bootstrap(config)
+      end
+      
+      def after_initialized(&block)
+        @initializer = AppBaseEngineInitializer.new if !@initializer
+        @initializer.after_initialized(&block)
       end
       
     end
@@ -87,26 +168,35 @@ module AppBase
   end
 
   class Railtie < Rails::Railtie
-
-    # default values for appbase configuration
-    config.appbase = ActiveSupport::OrderedOptions.new
-    config.appbase.enabled = false
-    config.appbase.mount = "/appbase"
-    config.appbase.user_identity = nil
-    config.appbase.token_store = :cookies # :cookies, :headers, :params
-    config.appbase.token_key_user = :u
-    config.appbase.token_key_session = :s
-    config.appbase.models = []
     
-    initializer "appbase.configure_route", :after => :add_routing_paths do |app|
-      
+    class << self
+      private
+      def setup_default(config)
+        return if config.respond_to? :appbase
+        # default values for appbase configuration
+        config.appbase = ActiveSupport::OrderedOptions.new
+        config.appbase.enabled = false
+        config.appbase.mount = "/appbase"
+        config.appbase.user_identity = nil
+        config.appbase.token_store = :cookies # :cookies, :headers, :params
+        config.appbase.token_key_user = :u
+        config.appbase.token_key_session = :s
+        config.appbase.models = []
+      end
+    end
+
+    def enabled
       if File.basename(ENV['_']) == 'rake'
         puts "Running with `rake #{$*.join(' ')}`"
       end
-      
-      if config.appbase.enabled && (File.basename(ENV['_']) != 'rake' || $*[0] == 'routes')
+      config.appbase.enabled && (File.basename(ENV['_']) != 'rake' || $*[0] == 'routes')
+    end
+    
+    setup_default config
+    
+    initializer "appbase.configure_route", :after => :add_routing_paths do |app|
+      if enabled
         AppBase::Engine.bootstrap app.config
-        
         app.routes.append do
           mount AppBase::Engine => Rails.application.config.appbase.mount
         end
